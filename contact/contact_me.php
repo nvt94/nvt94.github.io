4c8e23713968ebb628dba383c9ea00e650df92ca
Thanh Nguyen <thanh.nguyen3@sotatek.com>
2020-08-31 07:14:05 +0700

    master

--- contact/contact_me.php
+++ contact/contact_me.php
@@ -14,12 +14,12 @@ $email_address = $_POST['email'];
 $message = $_POST['message'];
 	
 // create email body and send it	
-$to = 'youremail@gmail.com'; // ----->>> put your email to receive mails
+$to = 'nvt94.ptit@gmail.com'; // ----->>> put your email to receive mails
 $email_subject = "Contact form submitted by:  $name";
 $email_body = "You have received a new message. \n\n".
 				  " Here are the details:\n \nName: $name \n ".
 				  "Email: $email_address\n Message \n $message";
-$headers = "From: admin@yoursite.com\n";
+$headers = "From: nvt94.ptit@gmail.com\n";
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
